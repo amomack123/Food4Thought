21d7e6eb51a87cf375faabaa2d0ad62fa5f402bb
matthewkim1005 <matt_hew_kim@yahoo.com>
2024-09-18 10:35:21 -0700

    Added Restaurant model and created DB for reviews.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    yelp_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=100)
+    favorited = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
